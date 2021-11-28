2cb788d4bf699c3f1c3920f5b334f2eb5d6a11b3
Matthew M <MatthewFMcAndrews@gmail.com>
2021-11-28 10:42:22 -0600

    format project files, remove deployment section from dproj

--- test/DataStructuresTest.dpr
+++ test/DataStructuresTest.dpr
@@ -19,52 +19,52 @@ uses
 
 {$IFNDEF TESTINSIGHT}
 var
-  runner: ITestRunner;
-  results: IRunResults;
-  logger: ITestLogger;
-  nunitLogger : ITestLogger;
+  Runner: ITestRunner;
+  Results: IRunResults;
+  Logger: ITestLogger;
+  NUnitLogger: ITestLogger;
 {$ENDIF}
 begin
   {$IFDEF TESTINSIGHT}
   TestInsight.DUnitX.RunRegisteredTests;
   {$ELSE}
   try
-    //Check command line options, will exit if invalid
+    { Check command line options, will exit if invalid }
     TDUnitX.CheckCommandLine;
-    //Create the test runner
-    runner := TDUnitX.CreateRunner;
-    //Tell the runner to use RTTI to find Fixtures
-    runner.UseRTTI := True;
-    //When true, Assertions must be made during tests;
-    runner.FailsOnNoAsserts := False;
+    { Create the test Runner }
+    Runner := TDUnitX.CreateRunner;
+    { Tell the Runner to use RTTI to find Fixtures }
+    Runner.UseRTTI := True;
+    { When true, Assertions must be made during tests }
+    Runner.FailsOnNoAsserts := False;
 
-    //tell the runner how we will log things
-    //Log to the console window if desired
-    if TDUnitX.Options.ConsoleMode <> TDunitXConsoleMode.Off then
-    begin
-      logger := TDUnitXConsoleLogger.Create(TDUnitX.Options.ConsoleMode = TDunitXConsoleMode.Quiet);
-      runner.AddLogger(logger);
+    { Tell the Runner how we will log things
+      Log to the console window if desired }
+    if TDUnitX.Options.ConsoleMode <> TDunitXConsoleMode.Off then begin
+      Logger := TDUnitXConsoleLogger.Create(TDUnitX.Options.ConsoleMode = TDunitXConsoleMode.Quiet);
+      Runner.AddLogger(Logger);
     end;
-    //Generate an NUnit compatible XML File
-    nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
-    runner.AddLogger(nunitLogger);
+    { Generate an NUnit compatible XML File }
+    NUnitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
+    Runner.AddLogger(NUnitLogger);
 
-    //Run tests
-    results := runner.Execute;
-    if not results.AllPassed then
+    { Run tests }
+    Results := Runner.Execute;
+    if not Results.AllPassed then begin
       System.ExitCode := EXIT_ERRORS;
+    end;
 
     {$IFNDEF CI}
-    //We don't want this happening when running under CI.
-    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
-    begin
+    { We don't want this happening when running under CI. }
+    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then begin
       System.Write('Done.. press <Enter> key to quit.');
       System.Readln;
     end;
     {$ENDIF}
   except
-    on E: Exception do
+    on E: Exception do begin
       System.Writeln(E.ClassName, ': ', E.Message);
+    end;
   end;
 {$ENDIF}
 end.
